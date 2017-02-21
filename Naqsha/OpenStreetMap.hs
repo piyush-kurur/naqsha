4a1b512d804ac5bede005483c84e2ee134d3da71
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-02-21 19:31:37 +0530

    hide all the unwanted meta lenses from top level module.

--- Naqsha/OpenStreetMap.hs
+++ Naqsha/OpenStreetMap.hs
@@ -4,5 +4,16 @@ module Naqsha.OpenStreetMap
       ( module Naqsha.OpenStreetMap.Element
       ) where
 
-import Naqsha.OpenStreetMap.Element hiding (unsafeToOsmID, OsmID)
+-- Hide all the meta data lenses.
+import Naqsha.OpenStreetMap.Element hiding (unsafeToOsmID, OsmID
+                                           , _osmID
+                                           , _modifiedUser
+                                           , _modifiedUserID
+                                           , _timeStamp
+                                           , _changeSet
+                                           , _version
+                                           , _isVisible
+                                           )
+
+-- Only expose the type from here.
 import Naqsha.OpenStreetMap.Element (OsmID)
